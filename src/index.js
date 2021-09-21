e92082b3603f2a9ea97858ab4404ff08023be6c8
talemIdir <idir.talem@gmail.com>
2021-09-21 15:48:35 +0100

    Forcing the page refresh after each navigation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter forceRefresh={true}>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
